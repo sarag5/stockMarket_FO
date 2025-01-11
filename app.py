5187d7ab556649aa8b7b58026be882a273778c33
Sabari Nathan <sabarinathan@Sabaris-MacBook-Air.local>
2025-01-11 17:48:23 +0100

    update the code for session

--- app.py
+++ app.py
@@ -74,25 +74,16 @@ def nearest_strike_nf(x):
     return round_nearest(x, 50)
 
 # API URLs and headers
-url_oc = "https://www.nseindia.com/option-chain"
+url_oc = "https://www.nseindia.com/"
 url_bnf = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
 url_nf = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
 url_indices = "https://www.nseindia.com/api/allIndices"
 
 headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'max-age=0',
-    'dnt': '1',
-    'sec-ch-ua': '"Chromium";v="131", "Not_A Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.9',
+    'referer': 'https://www.nseindia.com/option-chain',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
 }
 
 sess = requests.Session()
@@ -102,6 +93,7 @@ def set_cookie():
 
 def get_data(url):
     set_cookie()
+    time.sleep(2)
     response = sess.get(url, headers=headers, timeout=5)
     if response.status_code == 401:
         set_cookie()
